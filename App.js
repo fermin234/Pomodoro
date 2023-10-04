bf2d76e60d64f820a049d9fe9094746139f94448
fermin234 <fermin234@hotmail.com>
2023-10-04 11:22:20 -0300

    feacture pomodoro

--- App.js
+++ App.js
@@ -42,6 +42,12 @@ export default function App() {
       clearInterval(interval);
     }
 
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
     return () => {
       clearInterval(interval);
     };
